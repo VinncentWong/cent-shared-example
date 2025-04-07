8de9945cda81848c4527cc55f616d78e2adcee0d
VinncentWong <vinncentwong2@gmail.com>
2025-04-07 13:54:47 +0800

    Initial Commit

--- src/com/example/helper.groovy
+++ src/com/example/helper.groovy
@@ -4,9 +4,9 @@ public class Helper {
 
     void determineOddOrEven(int buildNumber) {
         if (buildNumber % 2 == 0) {
-            sh "echo Build Number: ${} is even"
+            sh "echo Build Number: ${buildNumber} is even"
         } else {
-            sh "echo Build Number: ${} is odd"
+            sh "echo Build Number: ${buildNumber} is odd"
         }
     }
 
